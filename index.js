3105d3d36a9354f2114c694001676a6164da0e90
bishnunathbd <bishnunath774@gmail.com>
2021-01-10 15:14:17 +0600

    51-db connection & dotenv setup

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbtxn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db(process.env.DB_NAME).collection("events");
+  console.log('db connected successfully!!!');
+  client.close();
+});
+
 
 app.listen(port)
